a8e0d7a357c1207859def5de4c2d919728396651
prageethk <karunadheera@gmail.com>
2020-05-19 23:44:08 +1000

    precision ADC - i2c communication client

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,40 @@
 
 #include "main.h"
 
-int max_pwm = 158;
+int max_pwm = 60;
 int min_pwm = -1;
 volatile int pwm = -1;
-int a3 = 0;
-int a2 = 0;
-int targetOutput = 727; // relates to around 59V
-int targetInput = 358; // 321:34V, 340:36V, 360:38V, 378:40V
-int loops = 0;
+volatile int targetInput = 540; // 38.8125V
+volatile int targetOutput = 560; // 59.2V
 
+volatile byte requestedAddr = 255;
+volatile int a3 = 0;
+volatile int a2 = 0;
+
+int loops = 0;
 
 void setup() {
 	Serial.begin(115200);
 	DDRC &= (1 << PC3); // ADC3 as input
 	DDRC &= (1 << PC2); // ADC2 as input
-	// PWM setup
+
 	DDRD |= (1 << PD3);
-	TCCR2A = 0x23;
-	TCCR2B = 0x09; // mode 7, clock prescale by 1
+	TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+	TCCR2B = _BV(WGM22) | _BV(CS20); // CS21:clk/8 32.25kHz, CS20:no prescaling 250kHz
 	setPWM();  // 160 clock periods = 10us per cycle
-	OCR2B = 0;
-	TCNT2 = 0;
+	OCR2A = 63;
+
+	Wire.setClock(100000);
+	Wire.begin(I2C_ADDR);
+	Wire.onReceive(i2cReceive);
+	Wire.onRequest(i2cSend);
 }
 
 void loop() {
-	int a3 = analogRead(A3);
-	int a2 = analogRead(A2);
-	_delay_ms(5);
-	if (a3 < 5 || a2 < 5) {
+	_delay_ms(10);
+	a3 = precisionAnalogRead(A3);
+	a2 = precisionAnalogRead(A2);
+	if (a2 < 142) {
 		decPWM();
 	} else {
 		if (a3 > targetOutput || a2 < targetInput) {
@@ -41,9 +47,74 @@ void loop() {
 	}
 
 	if (loops % 100 == 0) {
-		Serial.printf("A2:%d, A3:%d, PWM:%d\r\n", a2, a3, pwm);
+		Serial.printf("targetInput: %d, A2(In):%d, A3(Out):%d, PWM:%d\r\n",
+				targetInput, a2, a3, pwm);
 	}
 	loops++;
+}
+
+int precisionAnalogRead(uint8_t PORT) {
+	uint8_t x = 0;
+	int v = 0;
+	while (x++ < PRECISION_ANALOG_READ_COUNT) {
+		v = v + analogRead(PORT);
+	}
+	return v / PRECISION_ANALOG_READ_COUNT;
+}
+
+void i2cReceive(int k) {
+
+	byte addr = Wire.read();
+
+	if (I2C_ADDR_WRITE_TARGET_INPUT_ADC_VAL == addr) {
+		int n = ((byte) Wire.read()) | ((byte) Wire.read()) << 8;
+		if (n >= MIN_TARGET_INPUT && n <= MAX_TARGET_INPUT) {
+			targetInput = n;
+		}
+	} else if (I2C_ADDR_WRITE_TARGET_OUTPUT_ADC_VAL == addr) {
+		int n = ((byte) Wire.read()) | ((byte) Wire.read()) << 8;
+		if (n >= MIN_TARGET_OUTPUT && n <= MAX_TARGET_OUTPUT) {
+			targetOutput = n;
+		}
+	} else if (I2C_ADDR_WRITE_FREQ == addr) {
+		int n = ((byte) Wire.read()) | ((byte) Wire.read()) << 8;
+		if (n == 0) {
+			TCCR2B = _BV(WGM22) | _BV(CS20);
+		} else if (n == 1) {
+			TCCR2B = _BV(WGM22) | _BV(CS21);
+		}
+	} else {
+		requestedAddr = addr;
+	}
+}
+
+void i2cSend() {
+	if (I2C_ADDR_READ_TARGET_INPUT_ADC_VAL == requestedAddr) {
+		byte arr[2];
+		arr[0] = targetInput & 0xff;
+		arr[1] = (targetInput & 0xff00) >> 8;
+		Wire.write(arr, 2);
+	} else if (I2C_ADDR_READ_TARGET_OUTPUT_ADC_VAL == requestedAddr) {
+		byte arr[2];
+		arr[0] = targetOutput & 0xff;
+		arr[1] = (targetOutput & 0xff00) >> 8;
+		Wire.write(arr, 2);
+	} else if (I2C_ADDR_READ_INPUT_ADC_VAL == requestedAddr) {
+		byte arr[2];
+		arr[0] = a2 & 0xff;
+		arr[1] = (a2 & 0xff00) >> 8;
+		Wire.write(arr, 2);
+	} else if (I2C_ADDR_READ_OUTPUT_ADC_VAL == requestedAddr) {
+		byte arr[2];
+		arr[0] = a3 & 0xff;
+		arr[1] = (a3 & 0xff00) >> 8;
+		Wire.write(arr, 2);
+	} else if (I2C_ADDR_READ_PWM_VAL == requestedAddr) {
+		Wire.write(pwm);
+	} else if (I2C_ADDR_READ_FREQ == requestedAddr) {
+		Wire.write(TCCR2B == (_BV(WGM22) | _BV(CS20)) ? 0 : 1);
+	}
+	requestedAddr = 0xff;
 }
 
 void incPWM() {
@@ -52,7 +123,7 @@ void incPWM() {
 }
 
 void decPWM() {
-	pwm = pwm > min_pwm ? pwm -1 : pwm;
+	pwm = pwm > min_pwm ? pwm - 1 : pwm;
 	setPWM();
 }
 
@@ -62,7 +133,7 @@ void setPWM() {
 			DDRD &= ~(1 << PD3);
 		} else {
 			DDRD |= (1 << PD3);
-			OCR2A = 160 - pwm;
+			OCR2B = pwm;
 		}
 	}
 }
--- src/main.h
+++ src/main.h
@@ -7,11 +7,32 @@
 #ifndef _POWERWALL_MAIN_H_
 #define _POWERWALL_MAIN_H_
 #include <avr/io.h>
+#include <inttypes.h>
 #include <util/delay.h>
 #include <stdlib.h>
 #include <Arduino.h>
+#include <Wire.h>
 
-#define F_CPU  16000000L
+#define F_CPU  8000000L
+
+#define I2C_ADDR 0b00100010
+#define I2C_ADDR_WRITE_TARGET_INPUT_ADC_VAL 0
+#define I2C_ADDR_WRITE_TARGET_OUTPUT_ADC_VAL 4
+#define I2C_ADDR_READ_TARGET_INPUT_ADC_VAL 1
+#define I2C_ADDR_READ_TARGET_OUTPUT_ADC_VAL 5
+#define I2C_ADDR_READ_INPUT_ADC_VAL 2
+#define I2C_ADDR_READ_OUTPUT_ADC_VAL 3
+#define I2C_ADDR_READ_PWM_VAL 6
+#define I2C_ADDR_WRITE_FREQ 7
+#define I2C_ADDR_READ_FREQ 8
+
+#define MIN_TARGET_INPUT 140
+#define MAX_TARGET_INPUT 660
+
+#define MIN_TARGET_OUTPUT 200
+#define MAX_TARGET_OUTPUT 900
+
+#define PRECISION_ANALOG_READ_COUNT 4
 
 //end of add your includes here
 #ifdef __cplusplus
@@ -23,6 +44,9 @@ void loop();
 void incPWM();
 void decPWM();
 void setPWM();
+void i2cReceive(int k);
+void i2cSend();
+int precisionAnalogRead(uint8_t PORT);
 
 #ifdef __cplusplus
 } // extern "C"
