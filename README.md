3a97b4e8c910732dda54917e60a59ad5c24b2054
Prageeth Karunadheera <prageeth@lotj.com>
2021-08-20 19:03:41 +1000

    V2 - Move MPPT function to ATMEGEA, Add PCB design files

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ This project is intended to be used with below two projects.
 1. https://github.com/retromuz/esp32-bms-monitor.git
 2. https://github.com/retromuz/esp8266-powerwall.git
 
-This is the brain behind the DC to DC boost converter that charges 14S Li-Ion battery system. Acts as an I²C slave for ESP8266 master. ESP8266 manages the MPPT function with data provided by ESP32 connected to BMS.
+This is the brain behind the DC to DC boost converter that charges 14S Li-Ion battery system. It also does MPPT function. Acts as an I²C slave for ESP8266 master.
 
 
 ATMEGA328P drives boost converter at 125kHz. Duty cycle is varied at real-time to maintain output voltage within 14S battery charge limits. It also adjusts the input voltage (from 48V solar panels) according to MPPT instructions coming from ESP8266 to extract maximum energy during the day.
 
-![Powerwall - Web UI - Discharging](https://github.com/retromuz/atmega328p-powerwall/blob/master/photos/powerwall-web-ui-discharging.png?raw=true)
-![Schematics](https://github.com/retromuz/atmega328p-powerwall/blob/master/schematics/pcb-3D-view.png?raw=true)
-![Modding eBay 1500W boost converter - PCB Done](https://github.com/retromuz/atmega328p-powerwall/blob/master/photos/mod-boost-converter-pcb.png?raw=true)
-![Schematics](https://github.com/retromuz/atmega328p-powerwall/blob/master/schematics/schematic.png?raw=true)
+![PCB](https://github.com/retromuz/atmega328p-powerwall/blob/master/schematics/V2-pcb-2D-view.png?raw=true)
+![Schematics](https://github.com/retromuz/atmega328p-powerwall/blob/master/schematics/V2-schematic.png?raw=true)
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,30 @@
 
 #include "main.h"
 
-int max_pwm = 60;
-int min_pwm = -1;
-volatile int pwm = -1;
-volatile int targetInput = 560; // 40V
-//volatile int targetInput = 210; // temp
-volatile int targetOutput = 425; // 59.0V
+volatile int pwm = MIN_PWM;
+volatile unsigned int targetInput = MAX_TARGET_INPUT;
+volatile unsigned int targetOutput = CHARGE_STOP_VOLTAGE_ADC;
 
-volatile byte requestedAddr = 255;
-volatile int a3 = 0;
-volatile int a2 = 0;
-volatile int a1 = 0;
+volatile byte requestedAddr = I2C_CMD_DEFAULT;
+volatile unsigned int a3 = 0; // output voltage adc
+volatile unsigned int a2 = 0; // input voltage adc
+volatile unsigned int a1 = 0; // output current adc
 
-volatile int loops = 0;
+volatile unsigned long input_current = 0;
+volatile unsigned long output_current = 0;
+
+volatile unsigned int loops = 0;
 volatile unsigned int espRstLoops = 0;
 volatile byte espResetting = 0;
 
+volatile bool stopCharging = false;
+volatile bool noMppt = false;
+volatile bool mppt = true;
+volatile bool initMppt = true;
+MPPTData mpptData;
+
 void setup() {
-	Serial.begin(115200);
+//	Serial.begin(115200);
 	DDRC &= (1 << PC3); // ADC3 as input (output voltage measurement)
 	DDRC &= (1 << PC2); // ADC2 as input (input voltage measurement)
 	DDRC &= (1 << PC1); // ADC2 as input (output current sense)
@@ -37,46 +43,133 @@ void setup() {
 	DDRB |= (1 << PB0); // LED 3
 	DDRB |= (1 << PB1); // LED 4
 
-	Wire.setClock(100000);
+	Wire.setClock(I2C_CLOCK);
 	Wire.begin(I2C_ADDR);
 	Wire.onReceive(i2cReceive);
 	Wire.onRequest(i2cSend);
+	mpptData.status = 0;
 }
 
 void loop() {
-	a3 = precisionAnalogRead(A3);
-	a2 = precisionAnalogRead(A2);
-	a1 = precisionAnalogRead(A1);
-	if (a2 < 142) {
+
+	regulate();
+
+	PORTB &= ~(1 << PB0);
+
+	if (espResetting && espRstLoops > 4) {
+		PORTD |= (1 << PD7); // reset esp high
+		PORTB &= ~(1 << PB1); // turn off LED4
+		espResetting = 0;
+		espRstLoops = 0;
+	} else if (espRstLoops > ESP_RESET_LOOPS_COUNT_THRESHOLD) {
+		espRstLoops = 0;
+		resetESP8266();
+	}
+
+	if (!mppt && loops % (LOOPS_PER_SECOND * 60l * 10l) == 0) {
+		mppt = true;
+	}
+
+	if (mppt) {
+		mpptScan(initMppt ? 100 : 20);
+	}
+
+	loops++;
+	espRstLoops++;
+}
+
+void regulate() {
+	unsigned int a3x = precisionAnalogRead(A3); // output voltage adc
+	a2 = precisionAnalogRead(A2); // input voltage adc
+	a1 = precisionAnalogRead(A1); // output current adc
+	a3 = a3x - (a1 * 7 / 410);
+
+	output_current = (unsigned long) (a1 * OUTPUT_CURRENT_ADC_TO_MA_RATIO); // mA
+	unsigned long output_power = (unsigned long) (output_current
+			* ((58.8 * a3) / 420)); // mW
+	input_current = output_power / (a2 * 40 / 560); // mA
+
+	// cater for output sense voltage drift due to charge current
+
+	if (stopCharging) {
+		targetInput = MAX_TARGET_INPUT;
+	}
+
+	if (a2 < MIN_TARGET_INPUT || input_current > MAX_INPUT_CURRENT) {
 		decPWM();
 	} else {
-		if (a3 > targetOutput || a2 < targetInput) {
+		if (a3 > targetOutput || a2 < targetInput
+				|| ((a3 > CHARGE_STOP_VOLTAGE_ADC - 2)
+						&& output_current > MAX_OUTPUT_CURRENT_AT_SATURATION)
+				|| ((a3 > CHARGE_STOP_VOLTAGE_ADC - 1)
+						&& output_current > MAX_OUTPUT_CURRENT_AT_SATURATION / 2)) {
 			decPWM();
 		} else if (a3 < targetOutput && a2 > targetInput) {
 			incPWM();
 		}
 	}
 
-	if (loops % 100 == 0) {
-		Serial.printf("targetInput: %d, A2(In):%d, A3(Out):%d, PWM:%d\r\n",
-				targetInput, a2, a3, pwm);
+	if (a3 > CHARGE_STOP_VOLTAGE_ADC && a1 < CHAGE_STOP_CURRENT_ADC) {
+		stopCharging = true;
 	}
 
-	PORTB &= ~(1 << PB0);
-
-	if (espResetting && espRstLoops > 1000) {
-		PORTD |= (1 << PD7); // reset esp high
-		PORTB &= ~(1 << PB1); // turn off LED4
-		espResetting = 0;
-		espRstLoops = 0;
-	} else if (espRstLoops > 20000) {
-		espRstLoops = 0;
-		resetESP8266();
+	if (stopCharging && (a3 < CHARGE_RESTART_VOLTAGE_ADC)) {
+		stopCharging = false;
+		targetInput = TARGET_INPUT;
+		initMppt = true;
+		mppt = true;
 	}
 
-	loops++;
-	espRstLoops++;
+}
 
+void mpptScan(unsigned int statusesToCapture) {
+	if (noMppt) {
+		return;
+	}
+	if (mpptData.status == statusesToCapture) {
+		mpptData.status = 0;
+		mpptAnalyze(statusesToCapture);
+	} else if (loops % MPPT_STEP_DELAY_LOOPS == 0) {
+		if (mpptData.status == 0 && statusesToCapture == 20) {
+			unsigned int targetIn = targetInput + (MPPT_STEP * 10);
+			if (targetIn >= MIN_TARGET_INPUT && targetIn <= MAX_TARGET_INPUT) {
+				targetInput = targetIn;
+			}
+		}
+		regulate();
+		MPPTEntry entry;
+		entry.inAdc = targetInput;
+		entry.current = a1;
+		mpptData.data[mpptData.status] = entry;
+		mpptData.status++;
+		regulate();
+		unsigned int targetIn = targetInput - MPPT_STEP;
+		if (targetIn >= MIN_TARGET_INPUT && targetIn <= MAX_TARGET_INPUT) {
+			targetInput = targetIn;
+		}
+	}
+}
+
+void mpptAnalyze(unsigned int statusesToCapture) {
+	if (noMppt) {
+		return;
+	}
+	unsigned int max_current = 0;
+	unsigned int max_yield_inAdc = 0;
+	for (unsigned int x = 0; x < statusesToCapture; x++) {
+		regulate();
+		MPPTEntry entry = mpptData.data[x];
+		if (entry.current >= max_current) {
+			max_current = entry.current;
+			max_yield_inAdc = entry.inAdc;
+		}
+	}
+	if (max_yield_inAdc >= MIN_TARGET_INPUT
+			&& max_yield_inAdc <= MAX_TARGET_INPUT) {
+		targetInput = max_yield_inAdc;
+	}
+	mppt = false;
+	initMppt = false;
 }
 
 void resetESP8266() {
@@ -98,21 +191,21 @@ int precisionAnalogRead(uint8_t PORT) {
 
 void i2cReceive(int k) {
 	espRstLoops = 0;
-
 	PORTB |= (1 << PB0);
-
 	byte addr = Wire.read();
-
-	if (I2C_ADDR_WRITE_TARGET_INPUT_ADC_VAL == addr) {
+	if (I2C_CMD_WRITE_TARGET_INPUT_ADC_VAL == addr) {
 		int n = ((byte) Wire.read()) | ((byte) Wire.read()) << 8;
-		if (n >= MIN_TARGET_INPUT && n <= MAX_TARGET_INPUT) {
+		if (!stopCharging && n >= MIN_TARGET_INPUT && n <= MAX_TARGET_INPUT) {
 			targetInput = n;
 		}
-	} else if (I2C_ADDR_WRITE_TARGET_OUTPUT_ADC_VAL == addr) {
+	} else if (I2C_CMD_WRITE_TARGET_OUTPUT_ADC_VAL == addr) {
 		int n = ((byte) Wire.read()) | ((byte) Wire.read()) << 8;
 		if (n >= MIN_TARGET_OUTPUT && n <= MAX_TARGET_OUTPUT) {
 			targetOutput = n;
 		}
+	} else if (I2C_CMD_WRITE_NO_MPPT == addr) {
+		int n = ((byte) Wire.read()) | ((byte) Wire.read()) << 8;
+		noMppt = (n == 1);
 	} else {
 		requestedAddr = addr;
 	}
@@ -120,51 +213,65 @@ void i2cReceive(int k) {
 
 void i2cSend() {
 
-	if (I2C_ADDR_READ_TARGET_INPUT_ADC_VAL == requestedAddr) {
+	if (I2C_CMD_READ_TARGET_INPUT_ADC_VAL == requestedAddr) {
 		byte arr[2];
 		arr[0] = targetInput & 0xff;
 		arr[1] = (targetInput & 0xff00) >> 8;
 		Wire.write(arr, 2);
-	} else if (I2C_ADDR_READ_TARGET_OUTPUT_ADC_VAL == requestedAddr) {
+	} else if (I2C_CMD_READ_TARGET_OUTPUT_ADC_VAL == requestedAddr) {
 		byte arr[2];
 		arr[0] = targetOutput & 0xff;
 		arr[1] = (targetOutput & 0xff00) >> 8;
 		Wire.write(arr, 2);
-	} else if (I2C_ADDR_READ_INPUT_ADC_VAL == requestedAddr) {
+	} else if (I2C_CMD_READ_INPUT_ADC_VAL == requestedAddr) {
 		byte arr[2];
 		arr[0] = a2 & 0xff;
 		arr[1] = (a2 & 0xff00) >> 8;
 		Wire.write(arr, 2);
-	} else if (I2C_ADDR_READ_OUTPUT_ADC_VAL == requestedAddr) {
+	} else if (I2C_CMD_READ_OUTPUT_ADC_VAL == requestedAddr) {
 		byte arr[2];
 		arr[0] = a3 & 0xff;
 		arr[1] = (a3 & 0xff00) >> 8;
 		Wire.write(arr, 2);
-	} else if (I2C_ADDR_READ_OUTPUT_CURRENT_ADC_VAL == requestedAddr) {
+	} else if (I2C_CMD_READ_OUTPUT_CURRENT_ADC_VAL == requestedAddr) {
 		byte arr[2];
 		arr[0] = a1 & 0xff;
 		arr[1] = (a1 & 0xff00) >> 8;
 		Wire.write(arr, 2);
-	} else if (I2C_ADDR_READ_PWM_VAL == requestedAddr) {
+	} else if (I2C_CMD_READ_OUTPUT_CURRENT_ESTIMATE == requestedAddr) {
+		byte arr[3];
+		arr[0] = output_current & 0xff;
+		arr[1] = (output_current & 0xff00) >> 8;
+		arr[2] = (output_current & 0xff0000) >> 16;
+		Wire.write(arr, 3);
+	} else if (I2C_CMD_READ_INPUT_CURRENT_ESTIMATE == requestedAddr) {
+		byte arr[3];
+		arr[0] = input_current & 0xff;
+		arr[1] = (input_current & 0xff00) >> 8;
+		arr[2] = (input_current & 0xff0000) >> 16;
+		Wire.write(arr, 3);
+	} else if (I2C_CMD_READ_MPPT_STATUS == requestedAddr) {
+		Wire.write(noMppt ? 0 : 1);
+	} else if (I2C_CMD_READ_PWM_VAL == requestedAddr) {
 		Wire.write(pwm);
-	} else if (I2C_ADDR_READ_FREQ == requestedAddr) {
+	} else if (I2C_CMD_READ_FREQ == requestedAddr) {
 		Wire.write(TCCR2B == (_BV(WGM22) | _BV(CS20)) ? 0 : 1);
 	}
-	requestedAddr = 0xff;
+	requestedAddr = I2C_CMD_DEFAULT;
 }
 
 void incPWM() {
-	pwm = pwm < max_pwm ? pwm + 1 : pwm;
+	pwm = pwm < MAX_PWM ? pwm + 1 : pwm;
 	setPWM();
 }
 
 void decPWM() {
-	pwm = pwm > min_pwm ? pwm - 1 : pwm;
+	pwm = pwm > MIN_PWM ? pwm - 1 : pwm;
 	setPWM();
 }
 
 void setPWM() {
-	if (pwm >= min_pwm && pwm <= max_pwm) {
+	if (pwm >= MIN_PWM && pwm <= MAX_PWM) {
 		if (pwm == -1) {
 			DDRD &= ~(1 << PD3); // turn off output to set 0 duty cycle
 		} else {
